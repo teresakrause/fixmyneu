21bbc1b5a474bbfb650baa7b1cbe18ababa88616
teresakrause <krause.t@husky.neu.edu>
2014-10-15 15:58:54 -0400

    JS injection works, replacing images for links and banner from local files

--- myneu.js
+++ myneu.js
@@ -1,31 +1,42 @@
-$(document).ready(function(){
+jjn.src = "http://www.dailymobile.net/wp-content/uploads/2009/01/gmailicon9lpg.png";
+jjf.src = "http://www.dailymobile.net/wp-content/uploads/2009/01/gmailicon9lpg.png";
 
-var ext_url = chrome.extension.getURL('');
-var cat_image_url = ext_url + "images/cat.jpeg";
-var s = document.createElement('script');
-var jq = document.createElement('script');
-s.src = ext_url + "myneu.js";
-jq.src = ext_url + "jquery-2.1.1.min.js"
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head||document.documentElement).appendChild(jq);
-(document.head||document.documentElement).appendChild(s);
+hhn.src = "http://icons.iconarchive.com/icons/hopstarter/sleek-xp-basic/256/Folder-icon.png";
+hhf.src = "http://icons.iconarchive.com/icons/hopstarter/sleek-xp-basic/256/Folder-icon.png";
 
-//jjn = cat_image_url;
-//jjf = cat_image_url;
+iin.src = "http://emojipedia.org/wp-content/uploads/2014/04/128x128x1f4b0-google-android.png.pagespeed.ic.ZBTF2aFCY5.png"
+iif.src = "http://emojipedia.org/wp-content/uploads/2014/04/128x128x1f4b0-google-android.png.pagespeed.ic.ZBTF2aFCY5.png";
 
-$("img[title='mail']").attr("src", cat_image_url);
+//$("img[title='mail']").attr("src", jjn.src);
 
-$("img").parent("a").mouseenter(function() {
-    $(this).attr("src", cat_image_url);
-    console.log("mouseenter");
-});
-//$("img").parent("a").off("onmouseover");
-$("img").parent("a").mouseleave(function() {
-    $(this).attr("src", cat_image_url);
-    console.log("mouseleave");
-})
-//$("img").parent("a").off("onmouseout");
-
-});
+// $(document).ready(function(){
+//
+// var ext_url = chrome.extension.getURL('');
+// var cat_image_url = ext_url + "images/cat.jpeg";
+// var s = document.createElement('script');
+// var jq = document.createElement('script');
+// s.src = ext_url + "myneu.js";
+// jq.src = ext_url + "jquery-2.1.1.min.js"
+// s.onload = function() {
+//     this.parentNode.removeChild(this);
+// };
+// (document.head||document.documentElement).appendChild(jq);
+// (document.head||document.documentElement).appendChild(s);
+//
+// //jjn = cat_image_url;
+// //jjf = cat_image_url;
+//
+// $("img[title='mail']").attr("src", cat_image_url);
+//
+// $("img").parent("a").mouseenter(function() {
+//     $(this).attr("src", cat_image_url);
+//     console.log("mouseenter");
+// });
+// //$("img").parent("a").off("onmouseover");
+// $("img").parent("a").mouseleave(function() {
+//     $(this).attr("src", cat_image_url);
+//     console.log("mouseleave");
+// })
+// //$("img").parent("a").off("onmouseout");
+//
+// });
