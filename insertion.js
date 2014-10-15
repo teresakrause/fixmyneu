462e091c8985ccec85d3ccbcf85e42ad942bdc48
teresakrause <krause.t@husky.neu.edu>
2014-10-15 15:59:33 -0400

    new files added

--- insertion.js
+++ insertion.js
@@ -1,15 +1,25 @@
-//Inserts scripts into page source
-var ext_url = chrome.extension.getURL(''); //The url where this chrome extension's files live
-var s = document.createElement('script');
-s.src = ext_url + "myneu.js";
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
-//(document.head||document.documentElement).appendChild(jq);
-(document.head||document.documentElement).appendChild(s);
+var head = document.head || document.documentElement;
+
+//The url where this chrome extension's files live
+var ext_url = chrome.extension.getURL('');
+
+var script2 = document.createElement('script');
+script2.textContent = 'var ext_url = "' + ext_url + '";';
+head.appendChild(script2);
+
+var script1 = document.createElement('script');
+script1.src = ext_url + 'myneu.js';
+head.appendChild(script1);
+
+
+
 
 $(document).ready(function(){
+    //Replace link images
     $("img[title='mail']").attr("src", "http://www.dailymobile.net/wp-content/uploads/2009/01/gmailicon9lpg.png");
     $("img[title='my files']").attr("src", "http://icons.iconarchive.com/icons/hopstarter/sleek-xp-basic/256/Folder-icon.png");
     $("img[title='NUPay']").attr("src", "http://emojipedia.org/wp-content/uploads/2014/04/128x128x1f4b0-google-android.png.pagespeed.ic.ZBTF2aFCY5.png");
+
+    //Replace banner
+    $("img[title='Portal Logo']").attr("src", ext_url + "images/Northeastern_Univ_Logotype.png");
 })
--- myneu.js
+++ myneu.js
@@ -1,42 +1,8 @@
-jjn.src = "http://www.dailymobile.net/wp-content/uploads/2009/01/gmailicon9lpg.png";
-jjf.src = "http://www.dailymobile.net/wp-content/uploads/2009/01/gmailicon9lpg.png";
+jjn.src = ext_url + "images/mail_active.png";
+jjf.src = ext_url + "images/mail_inactive.png";
 
-hhn.src = "http://icons.iconarchive.com/icons/hopstarter/sleek-xp-basic/256/Folder-icon.png";
-hhf.src = "http://icons.iconarchive.com/icons/hopstarter/sleek-xp-basic/256/Folder-icon.png";
+hhn.src = ext_url + "images/myfiles_active.png";
+hhf.src = ext_url + "images/myfiles_inactive.png";
 
-iin.src = "http://emojipedia.org/wp-content/uploads/2014/04/128x128x1f4b0-google-android.png.pagespeed.ic.ZBTF2aFCY5.png"
-iif.src = "http://emojipedia.org/wp-content/uploads/2014/04/128x128x1f4b0-google-android.png.pagespeed.ic.ZBTF2aFCY5.png";
-
-//$("img[title='mail']").attr("src", jjn.src);
-
-// $(document).ready(function(){
-//
-// var ext_url = chrome.extension.getURL('');
-// var cat_image_url = ext_url + "images/cat.jpeg";
-// var s = document.createElement('script');
-// var jq = document.createElement('script');
-// s.src = ext_url + "myneu.js";
-// jq.src = ext_url + "jquery-2.1.1.min.js"
-// s.onload = function() {
-//     this.parentNode.removeChild(this);
-// };
-// (document.head||document.documentElement).appendChild(jq);
-// (document.head||document.documentElement).appendChild(s);
-//
-// //jjn = cat_image_url;
-// //jjf = cat_image_url;
-//
-// $("img[title='mail']").attr("src", cat_image_url);
-//
-// $("img").parent("a").mouseenter(function() {
-//     $(this).attr("src", cat_image_url);
-//     console.log("mouseenter");
-// });
-// //$("img").parent("a").off("onmouseover");
-// $("img").parent("a").mouseleave(function() {
-//     $(this).attr("src", cat_image_url);
-//     console.log("mouseleave");
-// })
-// //$("img").parent("a").off("onmouseout");
-//
-// });
+iin.src = ext_url + "images/nupay_active.png"
+iif.src = ext_url + "images/nupay_inactive.png";
