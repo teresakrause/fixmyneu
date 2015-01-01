76e750130d44005befa574779a9190bab236f585
teresakrause <krause.t@husky.neu.edu>
2014-12-31 23:03:08 -0800

    moved funtions to correct location

---
+++ functions.js
@@ -0,0 +1,19 @@
+//Make a button with a given image and link
+function makeAwesomeButton(fontname, link_url) {
+    var a =
+        "<a href='" + link_url + "'>" +
+        "<i class='fa fa-" + fontname + " fa-4x'></i>" +
+        "</a>";
+    return a;
+}
+
+//Remove minimize/ maximize buttons from each table
+
+/*
+function removeFocusButtons() {
+    $("img[src='/media/org/jasig/portal/layout/dlm/skins/classic/controls/focus.gif']").each(function(i, e) {
+        e.parentElement.parentElement.remove();
+    });
+}
+
+    */
