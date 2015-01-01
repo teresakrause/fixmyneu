6cf221bc48a53831f19685d845b0018f2cd8bfc3
teresakrause <krause.t@husky.neu.edu>
2015-01-01 01:53:25 -0800

    file name changes

--- homepage_init.js
+++ homepage_init.js
@@ -1,17 +1,8 @@
-//Make a button with a given image and link
-function makeAwesomeButton(fontname, link_url) {
-    var a =
-        "<a href='" + link_url + "'>" +
-        "<i class='fa fa-" + fontname + " fa-4x'></i>" +
-        "</a>";
-    return a;
-}
-
 $(document).ready(function(){
     //Replace link images
-    $("img[title='mail']").attr("src", "http://www.dailymobile.net/wp-content/uploads/2009/01/gmailicon9lpg.png");
-    $("img[title='my files']").attr("src", "http://icons.iconarchive.com/icons/hopstarter/sleek-xp-basic/256/Folder-icon.png");
-    $("img[title='NUPay']").attr("src", "http://emojipedia.org/wp-content/uploads/2014/04/128x128x1f4b0-google-android.png.pagespeed.ic.ZBTF2aFCY5.png");
+    $("img[title='mail']").attr("src", ext_url + "images/mail_inactive.png");
+    $("img[title='my files']").attr("src", ext_url + "images/myfiles_inactive.png");
+    $("img[title='NUPay']").attr("src", ext_url + "images/nupay_inactive.png");
 
     //Replace banner
     $("img[title='Portal Logo']").attr("src", ext_url + "images/myNEU_banner.png");
@@ -39,9 +30,5 @@ $(document).ready(function(){
     );
     $("td[class='bg1']").append(timesheetCell);
 
-
-    //Remove minimize/ maximize buttons from each table
-    $("img[src='/media/org/jasig/portal/layout/dlm/skins/classic/controls/focus.gif']").each(function(i, e) {
-        e.parentElement.parentElement.remove();
-    });
+    removeFocusButtons();
 })
---
+++ homepage_insertion.js
@@ -0,0 +1,14 @@
+var head = document.head || document.documentElement;
+
+//The url where this chrome extension's files live
+var ext_url = chrome.extension.getURL('');
+
+//Allow the imbedded js access to our directory
+var script2 = document.createElement('script');
+script2.textContent = 'var ext_url = "' + ext_url + '";';
+head.appendChild(script2);
+
+//Insert js to overwrite variables
+var script1 = document.createElement('script');
+script1.src = ext_url + 'myneu.js';
+head.appendChild(script1);
