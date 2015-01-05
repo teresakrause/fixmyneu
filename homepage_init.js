d7f583775150a1412df579546de1d351d28ab9a1
teresakrause <krause.t@husky.neu.edu>
2015-01-05 02:26:01 -0800

    moved bookmark bar to functions -- did not fix weird image height problem

--- homepage_init.js
+++ homepage_init.js
@@ -1,34 +1,10 @@
-$(document).ready(function(){
+ $(document).ready(function(){
     //Replace link images
     $("img[title='mail']").attr("src", ext_url + "images/mail_inactive.png");
     $("img[title='my files']").attr("src", ext_url + "images/myfiles_inactive.png");
     $("img[title='NUPay']").attr("src", ext_url + "images/nupay_inactive.png");
 
     //Replace banner
-    $("img[title='Portal Logo']").attr("src", ext_url + "images/myNEU_banner.png");
-
-    //Insert new exciting things into the header
-    //Link to COOL
-    var coolCell = makeAwesomeButton(
-        "suitcase",
-        "http://myneu.neu.edu/cp/ip/login?sys=pp&url=https://www.cool.neu.edu/students/mainmenu.aspx"
-    );
-
-    $("td[class='bg1']").append(coolCell);
-
-    // //Link to schedule
-    var scheduleCell = makeAwesomeButton(
-        "calendar-o",
-        "http://myneu.neu.edu/cp/ip/login?sys=sctssb&url=https://wl11gp.neu.edu/udcprod8/bwskfshd.P_CrseSchdDetl"
-    );
-    $("td[class='bg1']").append(scheduleCell);
-
-    // //Link to Timesheet
-    var timesheetCell = makeAwesomeButton(
-        "money",
-        "https://studentemployment.neu.edu/Tsx_StudentJobs.aspx?fo=true"
-    );
-    $("td[class='bg1']").append(timesheetCell);
-
+    $("img[title='Portal Logo']").replaceWith(makeBookmarkBar());
     removeFocusButtons();
 })
