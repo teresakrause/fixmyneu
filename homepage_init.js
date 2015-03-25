815be3d0cf2535a495c3972d4f2e8a9d858a1272
teresakrause <krause.t@husky.neu.edu>
2015-03-25 13:58:41 -0400

    located and highlighed withdrawl button

--- homepage_init.js
+++ homepage_init.js
@@ -4,6 +4,10 @@
     $("img[title='my files']").attr("src", ext_url + "images/myfiles_inactive.png");
     $("img[title='NUPay']").attr("src", ext_url + "images/nupay_inactive.png");
 
+    //Drop out of school
+    $dropout = $('b:contains("University Withdrawal Request")')
+    $dropout.addClass("withdraw-text").contents();
+
     //Replace banner
     makeBookmarkBar();
     removeFocusButtons();
